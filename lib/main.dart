36269a053f02e2bd96a46bf442b03bea93089b5c
Aburayas <tazshakas@gmail.com>
2024-06-10 07:38:38 +0300

    aburaya's touch no.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:s2/pages/invoices.dart';
+import 'package:s2/pages/pdfexport/pdf_previwe.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  InvoicePage(),
+      home:  PdfPreviewPage(),
     );
   }
 }
--- lib/pages/pdfexport/pdf_previwe.dart
+++ lib/pages/pdfexport/pdf_previwe.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:printing/printing.dart';
-import '../../models/invoice.dart';
 import 'pdf/pdfexport.dart';
 
 class PdfPreviewPage extends StatelessWidget {
-  final Invoice invoice;
-  const PdfPreviewPage({Key? key, required this.invoice}) : super(key: key);
+  const PdfPreviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
